bc96282f40adf4d800f825dd453df37a5455a5bb
Agobin Folefac <agobinfolefac@gmail.com>
2020-02-02 12:32:37 -0500

    Added the toolbar in the default activity

--- app/src/main/java/com/example/android/appbarexample/MainActivity.kt
+++ app/src/main/java/com/example/android/appbarexample/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.android.appbarexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import kotlinx.android.synthetic.main.toolbar_layout.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //This sets the cutomized toolbar created as the default action bar of this activity
+        setSupportActionBar(toolBar)
     }
 }
