1050949123c76ceec0bb436daa84f8a21266dc9d
Agobin Folefac <agobinfolefac@gmail.com>
2020-02-02 12:44:52 -0500

    Added a customized menu to the toolbar

--- app/src/main/java/com/example/android/appbarexample/MainActivity.kt
+++ app/src/main/java/com/example/android/appbarexample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.android.appbarexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.ActionBar
 import kotlinx.android.synthetic.main.toolbar_layout.*
 
@@ -13,5 +14,13 @@ class MainActivity : AppCompatActivity() {
 
         //This sets the cutomized toolbar created as the default action bar of this activity
         setSupportActionBar(toolBar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        var menuInflater = getMenuInflater()
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+
+        return true
     }
 }
